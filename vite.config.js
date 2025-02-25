38b59ff2861c0d83cafaa7251495f2a116df43fc
sramisetty1 <sramisetty1@miraclesoft.com>
2025-02-25 17:41:56 +0530

    modified vercel.json and vite.config

--- vite.config.js
+++ vite.config.js
@@ -10,5 +10,10 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     }
   },
- 
+  esbuild: {
+    legalComments: "none"
+  },
+  build: {
+    sourcemap: false
+  }
 })
